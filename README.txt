6eb63e0a721fa5e49706f898bd87255dcb6f9d95
Your Name <jsaxon@cars.com>
2011-09-03 11:05:40 -0700

    Hopefully this is a clean start to a Pyramid Project because the first thing I did was to grab .gitignore from https://github.com/Pylons/pyramid/blob/master/.gitignore

---
+++ README.txt
@@ -0,0 +1,1 @@
+backbone_fun README
---
+++ backbone_fun/views.py
@@ -0,0 +1,7 @@
+from backbone_fun.models import DBSession
+from backbone_fun.models import MyModel
+
+def my_view(request):
+    dbsession = DBSession()
+    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
+    return {'root':root, 'project':'backbone_fun'}
